e19da2c73e36c8d2edb6e36b61cbe8a01454edcc
David Odari <davidkibzodari@gmail.com>
2018-09-24 17:03:43 +0300

    [UPDATE] Updated Message Adapter Class redundant references

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
@@ -13,6 +13,7 @@ import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+
     public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
         super(context, resource, objects);
     }
@@ -23,9 +24,9 @@ public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
             convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
         }
 
-        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
-        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
-        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+        ImageView photoImageView = convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView =  convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView =  convertView.findViewById(R.id.nameTextView);
 
         FriendlyMessage message = getItem(position);
 
@@ -45,4 +46,5 @@ public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
 
         return convertView;
     }
+
 }
